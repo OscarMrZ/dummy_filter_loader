605b6b1304bba6a1699e877dc752ee6ee4a69fe7
oscarmartinez <oscar.martinez@pal-robotics.com>
2025-06-12 17:24:38 +0200

    Loading bug

--- src/filter_loader.cpp
+++ src/filter_loader.cpp
@@ -12,13 +12,15 @@ class DummyFilterLoader : public rclcpp::Node
 {
 public:
   DummyFilterLoader()
-  : Node("dummy_filter_loader"), loader_("filters",
+  : Node("dummy_filter_loader"), filter_loader_("filters",
       "filters::FilterBase<geometry_msgs::msg::WrenchStamped>")
   {
   }
 
   void configure_filter_chain()
   {
+    std::cout << "Configuring filter chain" << std::endl;
+
     filter_chain_ =
       std::make_unique<filters::FilterChain<geometry_msgs::msg::WrenchStamped>>(
       "geometry_msgs::msg::WrenchStamped");
@@ -28,19 +30,54 @@ public:
       this->get_node_parameters_interface());
   }
 
-  void configure_filter_directly()
+  void configure_filter_directly_unique_ptr()
   {
-    pluginlib::UniquePtr<filters::FilterBase<geometry_msgs::msg::WrenchStamped>> loaded_filter;
+    std::cout << "Configuring filter directly as a plugin (unique ptr)" << std::endl;
 
-    loaded_filter = loader_.createUniqueInstance("control_filters/GravityCompensationWrench");
-    loaded_filter->configure(
-      "sensor_filter_chain.filter1", "gravity_compensation",
+    pluginlib::UniquePtr<filters::FilterBase<geometry_msgs::msg::WrenchStamped>> filter;
+
+    std::string filter_type = "control_filters/GravityCompensationWrench";
+    filter = filter_loader_.createUniqueInstance(filter_type);
+
+    filter->configure(
+      "sensor_filter_chain.filter1.params", "gravity_compensation",
       this->get_node_logging_interface(), this->get_node_parameters_interface());
   }
 
+  void configure_filter_directly_as_plugin()
+  {
+    std::cout << "Configuring filter directly as a plugin (shared ptr)" << std::endl;
+
+    std::shared_ptr<filters::FilterBase<geometry_msgs::msg::WrenchStamped>> filter;
+    auto available_classes = filter_loader_.getDeclaredClasses();
+    std::cout << "available filters:" << std::endl;
+    for (const auto & available_class : available_classes) {
+      std::cout << "  " << available_class << std::endl;
+    }
+
+    std::string filter_type = "control_filters/GravityCompensationWrench";
+    filter = filter_loader_.createSharedInstance(filter_type);
+
+    filter->configure(
+      "sensor_filter_chain.filter1.params", "gravity_compensation",
+      this->get_node_logging_interface(), this->get_node_parameters_interface());
+  }
+
+  // void configure_filter_directly()
+  // {
+  //   std::cout << "Configuring filter directly" << std::endl;
+
+  //   std::shared_ptr<filters::FilterBase<geometry_msgs::msg::WrenchStamped>> filter =
+  //     std::make_shared<control_filters::GravityCompensation<geometry_msgs::msg::WrenchStamped>>();
+
+  //   filter->configure(
+  //     "sensor_filter_chain.filter1.params", "gravity_compensation",
+  //     this->get_node_logging_interface(), this->get_node_parameters_interface());
+  // }
+
 private:
   std::unique_ptr<filters::FilterChain<geometry_msgs::msg::WrenchStamped>> filter_chain_;
-  pluginlib::ClassLoader<filters::FilterBase<geometry_msgs::msg::WrenchStamped>> loader_;
+  pluginlib::ClassLoader<filters::FilterBase<geometry_msgs::msg::WrenchStamped>> filter_loader_;
 };
 
 int main(int argc, char * argv[])
@@ -48,7 +85,10 @@ int main(int argc, char * argv[])
   rclcpp::init(argc, argv);
 
   auto dummy_filter = std::make_shared<DummyFilterLoader>();
-  dummy_filter->configure_filter_directly();
+  dummy_filter->configure_filter_chain();
+  dummy_filter->configure_filter_directly_as_plugin();
+  dummy_filter->configure_filter_directly_unique_ptr();
+  // dummy_filter->configure_filter_directly();
 
   // rclcpp::spin(dummy_filter);
 
